e4c6cb0c672b6449225da99bebf131e74c4bda1a
Laura Gonzalez <laura.gonzalez@ias.com.co>
2023-02-04 09:23:51 -0500

    Calculadora terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCalculatorComponent } from './components/add-calculator/add-calculator.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SubtractCalculatorComponent } from './components/subtract-calculator/subtract-calculator.component';
+import { MultiplytCalculatorComponent } from './components/multiplyt-calculator/multiplyt-calculator.component';
+import { SplitCalculatorComponent } from './components/split-calculator/split-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCalculatorComponent,
+    SubtractCalculatorComponent,
+    MultiplytCalculatorComponent,
+    SplitCalculatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
